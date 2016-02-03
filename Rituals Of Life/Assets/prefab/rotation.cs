8ed2908502467817c23c2a63d55a2b8345a35128
moun3m <112365@edu.nhtv.nl>
2016-02-03 19:53:14 +0100

    Main_Menu
    Main_Menu, might've changed something in main-game, didn't check.

---
+++ Rituals Of Life/Assets/prefab/rotation.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class rotation : MonoBehaviour
+{
+	public float speed = 40f;
+
+
+	void Update ()
+	{
+		transform.Rotate(Vector3.up, speed * Time.deltaTime);
+		transform.Rotate(Vector3.left, speed/3 * Time.deltaTime);
+	}
+}
